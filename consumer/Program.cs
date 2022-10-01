65a386096a37d3fdc977a5873ca50f33af3914fb
Luke Bakken <luke@bakken.io>
2022-10-01 09:26:18 -0700

    Port over code from https://github.com/lukebakken/rabbitmq-dotnet-client-1060

---
+++ consumer/Program.cs
@@ -0,0 +1,81 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+using System.Text;
+
+AutoResetEvent latch = new AutoResetEvent(false);
+
+void CancelHandler(object? sender, ConsoleCancelEventArgs e)
+{
+    Console.WriteLine("CTRL-C pressed, exiting!");
+    e.Cancel = true;
+    latch.Set();
+}
+
+Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+
+Console.WriteLine("CONSUMER: waiting 5 seconds to try initial connection");
+Thread.Sleep(TimeSpan.FromSeconds(5));
+
+var factory = new ConnectionFactory()
+{
+    HostName = "rabbitmq",
+    Port = 5672
+};
+
+bool connected = false;
+
+IConnection? connection = null;
+
+while(!connected)
+{
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine("CONSUMER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+int message_count = 0;
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("CONSUMER: unexpected null connection");
+    }
+    else
+    {
+        using (var channel = connection.CreateModel())
+        {
+            channel.BasicQos(0, 5, false);
+
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("CONSUMER: waiting for messages...");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                DateTime received = DateTime.Now;
+                var body = ea.Body.ToArray();
+                string sentText = Encoding.ASCII.GetString(body);
+                DateTime sent = DateTime.ParseExact(sentText, "MM/dd/yyyy HH:mm:ss.ffffff", null);
+                TimeSpan delay = received - sent;
+                string receivedText = received.ToString("MM/dd/yyyy HH:mm:ss.ffffff");
+                Console.WriteLine($"CONSUMER received at {receivedText}, sent at {sentText} - count: {message_count++}, delay: {delay}");
+            };
+
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            latch.WaitOne();
+        }
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+using System.Text;
+
+Console.WriteLine("PRODUCER: waiting 5 seconds to try initial connection");
+Thread.Sleep(TimeSpan.FromSeconds(5));
+
+var factory = new ConnectionFactory()
+{
+    HostName = "rabbitmq",
+    Port = 5672
+};
+
+bool connected = false;
+
+IConnection? connection = null;
+
+while(!connected)
+{
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine("PRODUCER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("PRODUCER: unexpected null connection");
+    }
+    else
+    {
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        while (true)
+        {
+            string message = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.ffffff");
+            var body = Encoding.ASCII.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine($"PRODUCER sent {message}");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
+}
